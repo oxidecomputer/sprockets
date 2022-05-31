e86dc1f2a0394245215fd7af890e9fbec1ff74cd
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-05-31 12:50:17 -0400

    Add RotError::SpiError (#28)

--- common/src/msgs.rs
+++ common/src/msgs.rs
@@ -113,6 +113,7 @@ pub enum RotError {
     BadEncoding,
     UnsupportedVersion,
     InvalidOperation,
+    SpiError,
     GetCertificates(GetCertificatesError),
     AddHostMeasurements(AddHostMeasurementsError),
     GetMeasurements(GetMeasurementsError),
