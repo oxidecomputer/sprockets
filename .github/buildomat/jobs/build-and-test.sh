53c443d9ae7f3b8c39e2c9ed1e73c61142b23d23
Philip Tricca <flihp@twobit.org>
2025-07-06 15:14:04 -0700

    checkout test support binaries at specific revs
    Use the same `rev` as Cargo.toml where appropriate.
    Set default shell to `bash` so we can use the same character for line
    continuation on windows (where the default shell is `cmd) and linux.

--- .github/buildomat/jobs/build-and-test.sh
+++ .github/buildomat/jobs/build-and-test.sh
@@ -13,8 +13,13 @@ cargo --version
 rustc --version
 
 # dependencies used by `build.rs` to build test PKI, measurement log, & corpus
-cargo install --locked --git https://github.com/oxidecomputer/pki-playground
 cargo install --locked \
-    --git https://github.com/oxidecomputer/dice-util attest-mock
+    --git https://github.com/oxidecomputer/pki-playground \
+    --rev 0c121f9c1e84868e2331173107c2b7ed6f59b13a
+
+cargo install --locked \
+    --git https://github.com/oxidecomputer/dice-util \
+    --rev 4b408edc1d00f108ddf635415d783e6f12fe9641 \
+    attest-mock
 
 cargo test
